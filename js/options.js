19e77a0d93dda2927a19dd3316015eff5dc3e89c
zcochran <fuzzylimes@gmail.com>
2018-12-27 12:12:14 -0500

    Delete button is now working.
    TODO: Should not save null strings (don't add to set)

--- js/options.js
+++ js/options.js
@@ -1,30 +1,13 @@
-let reset = document.getElementById("clearRules");
+let clear = document.getElementById("clearRules");
 let save = document.getElementById("saveRules");
-let remove = document.getElementById("deleteString1");
+let remove = document.getElementsByClassName("delete");
 let add = document.getElementById("addRule");
 let cancel = document.getElementById("cancel");
-// let page = document.getElementById('buttonDiv');
-// const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1', '#fff'];
-// function constructOptions(kButtonColors) {
-//     for (let item of kButtonColors) {
-//         let button = document.createElement('button');
-//         button.style.backgroundColor = item;
-//         button.addEventListener('click', function () {
-//             chrome.storage.sync.set({ color: item }, function () {
-//                 console.log('color is ' + item);
-//             })
-//         });
-//         page.appendChild(button);
-//     }
-// }
-// constructOptions(kButtonColors);
+
 
 chrome.storage.sync.get('rules', function (data) {
     let savedRules = data['rules'];
     console.log(savedRules);
-    // if (Object.keys(savedRules).length > 0) {
-    //     console.log(Object.keys(savedRules).length);
-    // }
     let i = 1;
     for (let key in savedRules) {
         if (i > 1) {
@@ -37,7 +20,7 @@ chrome.storage.sync.get('rules', function (data) {
     }
 });
 
-reset.addEventListener('click', function() {
+clear.addEventListener('click', function() {
     if (confirm('Warning: This will permanently delete all of your saved filters.\nDo you wish to continue?')) {
         chrome.storage.sync.set({ state: false, rules: {} });
         location.reload();
@@ -52,11 +35,8 @@ save.addEventListener('click', function() {
     let rules = document.getElementsByClassName('rule');
     for (var rule of rules) {
         var elements = getStringsReplace(rule);
-        // let string1 = rule.getElementById("string1").value;
         let stringArray = elements.string.value.split('|');
         let set = new Set(stringArray);
-        // let replace1 = rule.getElementById("replace1").value;
-        // let val = { replace1: string1 };
         objectResponse[elements.replace.value] = [...set];
     }
 
@@ -65,18 +45,9 @@ save.addEventListener('click', function() {
     console.log(objectResponse);
 })
 
-remove.addEventListener('click', function(element) {
-    console.log(element)
-    let replace1 = document.getElementById("replace1").value;
-    chrome.storage.sync.get('rules', function(data) {
-        let savedRules = data['rules'];
-        console.log(savedRules);
-        console.log(savedRules[replace1]);
-        delete savedRules[replace1];
-        chrome.storage.sync.set({rules: savedRules});
-    });
-    location.reload();
-})
+for (let butt of remove) {
+    createEventListeners(butt);
+};
 
 add.addEventListener('click', function() {
     buildFields();
@@ -92,22 +63,39 @@ function buildFields() {
     var container = document.getElementsByClassName('container')[0];
     var newRule = rule.cloneNode(true);
 
-    // var string = newRule.getElementsByClassName('strings')[0];
-    // var replace = newRule.getElementsByClassName('replace')[0];
     var elements = getStringsReplace(newRule);
 
     elements.string.id = 'string' + (rules.length + 1);
     elements.replace.id = 'replace' + (rules.length +1);
     elements.string.value = '';
     elements.replace.value = '';
-    // var groups = newRule.getElementsByClassName('form-group');
-
 
     container.appendChild(newRule);
+    createEventListeners(newRule.getElementsByClassName('delete')[0]);
+    console.log(remove);
 }
 
 function getStringsReplace(element) {
     var string = element.getElementsByClassName('strings')[0];
     var replace = element.getElementsByClassName('replace')[0];
     return {string: string, replace: replace}
+}
+
+function clearFields(elements) {
+    elements.string.value = '';
+    elements.replace.value = '';
+}
+
+function createEventListeners(button) {
+    button.addEventListener('click', function (element) {
+        let rules = document.getElementsByClassName('rule');
+        var parentRule = element.currentTarget.parentNode.parentNode;
+
+        if (rules.length === 1) {
+            var elements = getStringsReplace(rules[0]);
+            clearFields(elements);
+        } else {
+            parentRule.remove();
+        }
+    })
 }
